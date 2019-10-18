46705792db3fc9bcb57f035d40740f197c855408
Milan Radojevic <mikister2012@gmail.com>
2019-10-18 19:37:08 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    };
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="header">
+          {/* menu button here */}
+          <h1>Minesweeper</h1>
+        </header>
+        <main className="grid">
+          <div className="cell"></div>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
