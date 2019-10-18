79ca1cf2e27f5819a25be6751caa95391814acda
Milan Radojevic <mikister2012@gmail.com>
2019-10-18 22:01:38 +0200

    Add cell mechanism

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './Cell.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBomb } from '@fortawesome/free-solid-svg-icons'
+
 class Cell extends Component {
   constructor(props) {
     super(props);
@@ -9,9 +12,19 @@ class Cell extends Component {
     };
   }
 
+  onCellClick() {
+    this.setState({
+      revealed: true
+    });
+
+    // Emit event that cell is clicked wiht cell ID
+  }
+
   render() {
     return(
-      <div className="cell"></div>
+      <div className={"cell " + (this.state.revealed ? "cell--revealed " : "") } onClick={this.onCellClick.bind(this)}>
+        <FontAwesomeIcon icon={faBomb} />
+      </div>
     );
   }
 }
