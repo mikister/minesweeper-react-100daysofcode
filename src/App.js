ab0b41b46cb88e3b38aca120cc2cf088311cfc70
Milan Radojevic <mikister2012@gmail.com>
2019-10-18 20:13:50 +0200

    Add cell grid

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Cell from "./components/Cell.js";
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      colsNum: 10,
+      rowsNum: 12,
     };
+
+    this.gridRef = React.createRef();
   }
+
+  componentDidMount() {
+    this.gridRef.current.style.setProperty("--colsNum", this.state.colsNum);
+  }
+
+  renderGrid() {
+    var cells = [];
+
+    for(var ii = 1; ii < (this.state.colsNum * this.state.rowsNum); ii++) {
+      cells.push(<Cell />);
+    }
+
+    return cells;
+  }
+  
 
 
   render() {
@@ -17,7 +37,8 @@ class App extends Component {
           {/* menu button here */}
           <h1>Minesweeper</h1>
         </header>
-        <main className="grid">
+        <main className="grid" ref={this.gridRef}>
+          { this.renderGrid.bind(this)() }
           <div className="cell"></div>
         </main>
       </div>
