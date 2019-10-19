b1652762d864ac0747f7d81cae673318b08075de
Milan Radojevic <mikister2012@gmail.com>
2019-10-19 13:55:24 +0200

    Add cell content

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -20,10 +20,22 @@ class Cell extends Component {
     // Emit event that cell is clicked wiht cell ID
   }
 
+  renderContent() {
+    if(this.props.content === "") {
+      return( null );
+    }
+    else if(this.props.content === "b") {
+      return( <FontAwesomeIcon icon={faBomb} /> );
+    }
+    else {
+      return( <span>{ this.props.content }</span> );
+    }
+  }
+
   render() {
     return(
       <div className={"cell " + (this.state.revealed ? "cell--revealed " : "") } onClick={this.onCellClick.bind(this)}>
-        <FontAwesomeIcon icon={faBomb} />
+        { this.renderContent() }
       </div>
     );
   }
